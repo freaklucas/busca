e4fcdb41b9faf0ee5558b634aef76ffee780dd6a
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-21 17:33:36 -0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+
+import api from "./services/api";
+
 import "./styles.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  function handleSearch() {
+    //01310930/json/
+    if (input === "") {
+      alert("Preencha o cep!");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      console.log(response);
+    } catch {
+      alert("Erro ao buscar");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Busca CEP</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite o Cep..." />
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Digite o Cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
